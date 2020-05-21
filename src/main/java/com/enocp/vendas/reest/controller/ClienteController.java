9182291542bdb584ed2d3085528c5875d18aac50
Enocp <pierre.enoc@gmail.com>
2020-05-21 13:31:27 -0500

    requisição get com praramentros e respons entity

--- src/main/java/com/enocp/vendas/reest/controller/ClienteController.java
+++ src/main/java/com/enocp/vendas/reest/controller/ClienteController.java
@@ -1,24 +1,35 @@
 package com.enocp.vendas.reest.controller;
 
+import ch.qos.logback.core.net.server.Client;
+import com.enocp.vendas.domain.entity.Cliente;
+import com.enocp.vendas.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/api/cliente")
+//@RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(
-            value = "/hello/{nome}",
-            method = RequestMethod.POST,
-            consumes = {"application/json", "application/xml"},
-            produces = {"application/json", "application/xml"}
-     )
-    @ResponseBody
-    public String helloCLiente( @PathVariable("nome") String nomeCliente) {
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
 
-        return  String.format("Hello %s ", nomeCliente);
+    private Clientes clientes;
+
+    @GetMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity getClienteById( @PathVariable  Integer id){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+          return ResponseEntity.ok(cliente.get());
+
+        }
+            return ResponseEntity.notFound().build();
     }
 }
+
